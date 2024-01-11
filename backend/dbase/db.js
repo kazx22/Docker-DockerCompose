64345848bc3a424591086596d9d1c47a24d7fe76
kazi <kazialifhaider@gmail.com>
2024-01-11 08:10:35 +0000

    Authentication Failed

--- backend/dbase/db.js
+++ backend/dbase/db.js
@@ -2,13 +2,15 @@ const mongoose = require("mongoose");
 
 const MONGODB_URI = "mongodb://mongodb:27017/portfolio";
 
-// Connect to MongoDB
+// const MONGODB_URI =
+//   "mongodb://admin:pass@database_c:27017/portfolio?authSource=myAuthDb";
+
 const connectDB = async () => {
   try {
     await mongoose.connect(MONGODB_URI, {});
     console.log("Database Connected");
   } catch (err) {
-    console.err(err);
+    console.error(err);
   }
 };
 
